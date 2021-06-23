c61057886ce6a86af0727127806537798c236d1a
Tadeu Agostini <tadeu.agostini@folhacerta.com>
2021-06-22 23:52:09 -0300

    :tada: initial conf

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    ['@babel/preset-react', {
+      runtime: 'automatic'
+    }]
+  ],
+  plugins: [
+    "babel-plugin-styled-components",
+    [
+      "babel/plugin-transform-runtime",
+      {
+        "regenerator": true
+      }
+    ]
+  ]
+}
